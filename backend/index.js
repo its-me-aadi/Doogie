b6c77b55a5ed06dba7c5ee732ae9b400e948eba2
Aditya <itsadityasharma7124@gmail.com>
2024-07-03 17:13:19 +0530

    backend server

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ mongoDB();
 
 
 app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin","https://doogiie.netlify.app");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin,X-Requested-With,Content-Type,Accept"
